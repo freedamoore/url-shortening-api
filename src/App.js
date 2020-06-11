e2aff61399c28c7c8e844da26614f66f46fa833b
Freeda Moore <freedamoore@outlook.com>
2020-06-11 11:45:57 +1000

    added the main functionality of inputing a url and using the API to return a short link. Implemented this using Hooks useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [shortUrl, setShortUrl] = useState('');
+  const [inputUrl, setInputUrl] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //TODO: need to handle invalid urls here....
+    setInputUrl(document.getElementById('url').value);
+  };
+
+  useEffect(() => {
+    if (inputUrl.length > 0){
+        fetch('https://rel.ink/api/links/', {
+        method: 'POST',
+        body: JSON.stringify({
+          url: `${inputUrl}`
+        }),
+        headers: {
+          "Content-type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(json => setShortUrl(`https://rel.ink/${json.hashid}`))
+      .catch((error) => console.error('Error:', error));
+    }
+  }, [inputUrl]);
+
   return (
     <div className="App">
       <h1>Shortly</h1>
+      <form>
+        <input type='url' id='url'></input>
+        <input type="submit" value="Shorten it!" onClick={ handleSubmit }></input>
+      </form>
+      <p>{inputUrl}</p>
+      <p>{shortUrl}</p>
     </div>
   );
 }
