a0cdbcaaf8faaec2fb3edde4d84c39e37112aafb
Freeda Moore <freedamoore@outlook.com>
2020-06-17 11:22:33 +1000

    Updates

---
+++ precache-manifest.f1f4c15fd18a1b15f4e948d128a3ef5d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c39976fa0d8d0022a94b307ed893001",
+    "url": "/url-shortening-api/index.html"
+  },
+  {
+    "revision": "430b4cb1138b31e5844d",
+    "url": "/url-shortening-api/static/css/main.a25c1e61.chunk.css"
+  },
+  {
+    "revision": "3c37560eedfaae8c6dfa",
+    "url": "/url-shortening-api/static/js/2.4dd73209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/url-shortening-api/static/js/2.4dd73209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430b4cb1138b31e5844d",
+    "url": "/url-shortening-api/static/js/main.8e4da607.chunk.js"
+  },
+  {
+    "revision": "5584b08adf4018e35088",
+    "url": "/url-shortening-api/static/js/runtime-main.19a959de.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/url-shortening-api/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/url-shortening-api/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/url-shortening-api/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "4a7a7f615fb37565a0a706a7379dfb7e",
+    "url": "/url-shortening-api/static/media/bg-shorten-mobile.4a7a7f61.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/url-shortening-api/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/url-shortening-api/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/url-shortening-api/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/url-shortening-api/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/url-shortening-api/static/media/logo.6387840f.svg"
+  }
+]);
